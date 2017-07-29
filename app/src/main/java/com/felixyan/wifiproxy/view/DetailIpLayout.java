00c405e5049d7a8047639e1e20183f6682f273b5
mybuddy <felixyan@outlook.com>
2017-07-30 01:15:39 +0800

    完成IP设置页UI数据加载

--- app/src/main/java/com/felixyan/wifiproxy/view/DetailIpLayout.java
+++ app/src/main/java/com/felixyan/wifiproxy/view/DetailIpLayout.java
@@ -1,17 +1,23 @@
 package com.felixyan.wifiproxy.view;
 
 import android.content.Context;
+import android.net.wifi.WifiConfiguration;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
 
 import com.felixyan.wifiproxy.R;
+import com.felixyan.wifiproxy.StaticIpConfiguration;
+import com.felixyan.wifiproxy.WifiCenter;
 import com.felixyan.wifiproxy.adapter.IViewWrapper;
-import com.felixyan.wifiproxy.model.IpInfo;
 import com.felixyan.wifiproxy.model.WifiItemData;
+
+import java.net.InetAddress;
+import java.util.ArrayList;
 
 /**
  * Created by yanfei on 2017/7/21.
@@ -20,6 +26,11 @@ import com.felixyan.wifiproxy.model.WifiItemData;
 public class DetailIpLayout extends LinearLayout implements IViewWrapper<WifiItemData>{
     private Spinner mSpIp;
     private View mPrlStaticIp;
+    private EditText mEtIpAddress;
+    private EditText mEtGateway;
+    private EditText mEtPrefixLength;
+    private EditText mEtDns1;
+    private EditText mEtDns2;
 
     public DetailIpLayout(Context context) {
         super(context);
@@ -47,13 +58,20 @@ public class DetailIpLayout extends LinearLayout implements IViewWrapper<WifiIte
         inflate(context, R.layout.layout_detail_ip, this);
         mSpIp = (Spinner) findViewById(R.id.spIP);
         mPrlStaticIp = findViewById(R.id.prlStaticIp);
+        mEtIpAddress = (EditText) findViewById(R.id.etIpAddress);
+        mEtGateway = (EditText) findViewById(R.id.etGateway);
+        mEtPrefixLength = (EditText) findViewById(R.id.etPrefixLength);
+        mEtDns1 = (EditText) findViewById(R.id.etDns1);
+        mEtDns2 = (EditText) findViewById(R.id.etDns2);
 
         mSpIp.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 if(position == 0) {
+                    // DHCP
                     mPrlStaticIp.setVisibility(GONE);
                 } else if (position == 1) {
+                    // Static
                     mPrlStaticIp.setVisibility(VISIBLE);
                 }
             }
@@ -72,6 +90,30 @@ public class DetailIpLayout extends LinearLayout implements IViewWrapper<WifiIte
 
     @Override
     public void setData(int position, WifiItemData data) {
-        IpInfo info = new IpInfo();
+        //IpInfo info = new IpInfo();
+        if(data.isConnected() || data.isSaved()) {
+            WifiConfiguration config = WifiCenter.getInstance(getContext()).getSsidConfig(data.getSsid());
+            if(config != null) {
+                WifiCenter.IpAssignment ipAssignment = WifiCenter.getIpAssignment(config);
+                if(ipAssignment == WifiCenter.IpAssignment.STATIC) {
+                    mSpIp.setSelection(1);
+                    StaticIpConfiguration staticIpConfiguration = WifiCenter.getStaticIpConfiguration(config);
+                    if(staticIpConfiguration != null) {
+                        mEtIpAddress.setText(staticIpConfiguration.getIpAddress().getAddress().getHostAddress());
+                        mEtGateway.setText(staticIpConfiguration.getGateway().getHostAddress());
+                        mEtPrefixLength.setText(staticIpConfiguration.getIpAddress().getPrefixLength());
+                        ArrayList<InetAddress> dnsServers = staticIpConfiguration.getDnsServers();
+                        if(!dnsServers.isEmpty()) {
+                            mEtDns1.setText(dnsServers.get(0).getHostAddress());
+                            if(dnsServers.size() > 1) {
+                                mEtDns2.setText(dnsServers.get(1).getHostAddress());
+                            }
+                        }
+                    }
+                } else if (ipAssignment == WifiCenter.IpAssignment.DHCP) {
+                    mSpIp.setSelection(0);
+                }
+            }
+        }
     }
 }
